f1a20a028de65df046967791df44c5cb87e9912e
Amany Mohamed <147101273+Eng-AmanyMohamed@users.noreply.github.com>
2024-12-19 22:07:59 +0200

    Update IAssessmentService.java

--- src/main/java/com/lms/LearningManagementSystem/Service/IAssessmentService.java
+++ src/main/java/com/lms/LearningManagementSystem/Service/IAssessmentService.java
@@ -1,5 +1,4 @@
 package com.lms.LearningManagementSystem.Service;
-
 import com.lms.LearningManagementSystem.Model.Assessment.*;
 import java.util.*;
 
@@ -15,9 +14,8 @@ public interface IAssessmentService {
     void submitAssignment(Long assignmentId, String fileName, Long studID);
     Assignment findAssignmentById(Long id);
     List<Assignment> GetAllAssignments();
-    void gradeAssignment(Long studentId, Long assessmentId, int marks, String feedback);
-    List<Grading> getGradingsForStudent(Long studentId);
-    Map<String, Object> trackStudentPerformance(Long studentId); //for track assignments and quizzes for specific student
-    List<Assignment> trackStudentAssignments(Long studentId); //for track assignments for specific student
+    void gradeAssignment(Long studentId, String type, String marks, String feedback);
+    List<Grading> trackStudentPerformance(Long studentId);  //for track assignments and quizzes for specific student
+    List<Grading> trackStudentAssignments(Long studentId); //for track assignments for specific student
     List<Grading> trackStudentQuizPerformance(Long studentId);//for track  quizzes for specific student
 }
