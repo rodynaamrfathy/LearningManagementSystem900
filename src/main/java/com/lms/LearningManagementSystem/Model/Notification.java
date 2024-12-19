047f40565079a57eb80d54405a3228ba5b9fca2d
Asmaa Yasser Hussein <114663258+asmaa465@users.noreply.github.com>
2024-12-19 02:55:57 +0200

    Add files via upload

---
+++ src/main/java/com/lms/LearningManagementSystem/Model/Notification.java
@@ -0,0 +1,31 @@
+package com.lms.LearningManagementSystem.Model;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Notification {
+    private String id;
+    private String message;
+    private boolean read;
+
+    public Notification(String id, String message) {
+        this.id = id;
+        this.message = message;
+        this.read = false;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public void markAsRead() {
+        this.read = true;
+    }
+}
